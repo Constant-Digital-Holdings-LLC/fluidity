f1727901f8da1a0e819f2dff0bd1bbcbe1248cea
Sean Lynch <sean@constantdigital.com>
2022-09-15 15:31:42 -0700

    playing around with TS

--- service/dist/server/app.js
+++ service/dist/server/app.js
@@ -1,3 +1,30 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const ring_buffer_ts_1 = require("ring-buffer-ts");
 console.log('test from server -- from TS');
-//3
+const app = (0, express_1.default)();
+const port = 3000;
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.get('/', async (req, res) => {
+    return res.status(200).send({
+        message: 'Hello World!'
+    });
+});
+try {
+    app.listen(port, () => {
+        console.log(`Connected successfully on port ${port}`);
+    });
+}
+catch (err) {
+    console.error(err);
+}
+const ringBuffer = new ring_buffer_ts_1.RingBuffer(5);
+ringBuffer.add(1);
+ringBuffer.add(2, 3);
+ringBuffer.add(4, 5, 6);
+console.log(ringBuffer.toArray());
--- service/src/server/app.ts
+++ service/src/server/app.ts
@@ -1,2 +1,31 @@
+import express, { Application, Request, Response } from 'express';
+import { RingBuffer } from 'ring-buffer-ts';
+
 console.log('test from server -- from TS');
-//3
+
+const app: Application = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async (req: Request, res: Response): Promise<Response> => {
+    return res.status(200).send({
+        message: 'Hello World!'
+    });
+});
+
+try {
+    app.listen(port, (): void => {
+        console.log(`Connected successfully on port ${port}`);
+    });
+} catch (err) {
+    console.error(err);
+}
+
+const ringBuffer = new RingBuffer<number>(5);
+ringBuffer.add(1);
+ringBuffer.add(2, 3);
+ringBuffer.add(4, 5, 6);
+
+console.log(ringBuffer.toArray());
