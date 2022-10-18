6752b892d54182bc066716e4cf84cb3e6a55b429
Sean Lynch <sean@constantdigital.com>
2022-10-18 16:16:55 -0700

    testing

--- service/dist/agent/app.js
+++ service/dist/agent/app.js
@@ -1,6 +1,8 @@
 import { loggerUtility } from '#@shared/modules/logger.js';
+import { config } from '#@shared/modules/config.js';
 const log = await loggerUtility;
 log.debug('this is debug data');
 log.info('this is info data');
 log.warn('this is warn data');
 log.error('this is error data');
+log.debug(`conf: ${JSON.stringify(await config)}`);
--- service/src/agent/app.ts
+++ service/src/agent/app.ts
@@ -7,3 +7,5 @@ log.debug('this is debug data');
 log.info('this is info data');
 log.warn('this is warn data');
 log.error('this is error data');
+
+log.debug(`conf: ${JSON.stringify(await config)}`);
