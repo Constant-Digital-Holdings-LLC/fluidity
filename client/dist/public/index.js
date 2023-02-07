16ef8756c171942e4d8276641ebec19cd6c51be7
Sean Lynch <sean@constantdigital.com>
2023-02-06 17:57:23 -0800

    added lunr support

--- client/dist/public/index.js
+++ client/dist/public/index.js
@@ -1,6 +1,7 @@
 import { fetchLogger } from '#@shared/modules/logger.js';
 import { confFromDOM } from '#@shared/modules/fluidityConfig.js';
 import { FluidityClient } from './modules/fluidityClient.js';
+const lunr = window.lunr;
 const conf = confFromDOM();
 if (!conf)
     throw new Error('Missing Fluidity Client Config');
@@ -8,4 +9,26 @@ const log = fetchLogger(conf);
 log.debug(conf);
 const fc = new FluidityClient();
 fc.sayHi();
+var documents = [
+    {
+        name: 'Lunr',
+        text: 'Like Solr, but much smaller, and not as bright.'
+    },
+    {
+        name: 'React',
+        text: 'A JavaScript library for building user interfaces.'
+    },
+    {
+        name: 'Lodash',
+        text: 'A modern JavaScript utility library delivering modularity, performance & extras.'
+    }
+];
+var idx = lunr(function () {
+    this.ref('name');
+    this.field('text');
+    documents.forEach(function (doc) {
+        this.add(doc);
+    }, this);
+});
+log.info(idx.search('bright'));
 //# sourceMappingURL=index.js.map
